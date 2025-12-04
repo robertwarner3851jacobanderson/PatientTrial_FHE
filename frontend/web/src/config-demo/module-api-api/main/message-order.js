5eda3c5ac873e805bc7cdba4568c858e49228a79
robertwarner3851jacobanderson <olyap830724424@rambler.ru>
2025-12-04 11:56:27 +0800

    add EthereumConfig.json, message-order.js, fragments-template-utils.java

---
+++ frontend/web/src/config-demo/module-api-api/main/message-order.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const global-provider-sms = () => { return null; };
---
+++ frontend/web/src/notification-configuration/customer-validator-email/fragments-template-utils.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\notification-configuration\customer-validator-email\fragments-template-utils.java
+// Generated at: 2025-10-10T05:50:58.863Z
